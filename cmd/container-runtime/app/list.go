2b30d39ae08393ea32151314d7d8d15345e5e6f6
duyanghao <1294057873@qq.com>
2020-11-05 11:53:45 +0800

    add container start
    Signed-off-by: duyanghao <1294057873@qq.com>

--- cmd/container-runtime/app/list.go
+++ cmd/container-runtime/app/list.go
@@ -34,7 +34,7 @@ func ListContainers() {
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
-	fmt.Fprint(w, "ID\tNAME\tPID\tSTATUS\tCOMMAND\tCREATED\n")
+	fmt.Fprint(w, "ID\tIMAGE\tNAME\tPID\tSTATUS\tCOMMAND\tCREATED\n")
 	for _, item := range containers {
 		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
 			item.Id,
